5ca818694b37baecf146b9052dd1585828950796
Sohan <sohanz.singh2@gmail.com>
2020-12-02 23:24:09 +0530

    Created search component and added state to parent App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import Cardlist from './Cardlist';
 import {robots} from './robots';
+import SearchBox from './SearchBox';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            robots : robots,
+            searchField : "",
+        }
+        this.onSearchChange = this.onSearchChange.bind(this);
+    };
+
+    onSearchChange(event) {
+        this.setState({
+                searchField: event.target.value,
+            }
+        )
+    }
+    
     render() {
-        return <Cardlist robots = {robots}/>;
+        const cards = this.state.robots.filter((robot) => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
+        return (
+            <div className = 'tc'>
+                <h2>Robofriends</h2>
+                <SearchBox search = {this.onSearchChange}/>
+                <Cardlist robots = {cards}/>
+            </div>
+        );
     }
 }
 
---
+++ src/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function SearchBox(props) {
+    const {search} = props;
+    return (
+        <div className = 'pa2'>
+            <input
+                className = 'pa2 ba b--green blue'
+                type="search" 
+                placeholder="search friends"
+                onChange = {search}
+                />
+        </div>
+    );
+}
+
+export default SearchBox;
